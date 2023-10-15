5860b5c1cda3f636f075f5107a2aef500620b6ab
Nick Edwards <46484451+nickjedwards@users.noreply.github.com>
2023-10-15 15:43:25 +1100

    init

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,39 @@
+local plugins = {
+  {
+    'christoomey/vim-tmux-navigator',
+    Lazy = false
+  },
+  {
+    'mhartington/formatter.nvim',
+    event = 'VeryLazy',
+    opts = function ()
+      return require 'custom.configs.formatter'
+    end
+  },
+  {
+    'mfussenegger/nvim-lint',
+    event = 'VeryLazy',
+    config = function ()
+      require 'custom.configs.lint'
+    end
+  },
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'eslint-lsp',
+        'prettier',
+        'typescript-language-server'
+      }
+    }
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function ()
+    require 'plugins.configs.lspconfig'
+    require 'custom.configs.lspconfig'
+    end,
+  },
+}
+
+return plugins
---
+++ README.md
@@ -0,0 +1,23 @@
+# Dotfiles
+
+## Alacritty
+
+`sudo apt install alacritty`
+
+## Zsh && Oh My ZSH!
+
+`sudo apt install zsh`
+
+`chsh -s $(which zsh)`
+
+`sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+
+## Tmux
+
+`sudo apt install tmux`
+
+## Neovim && NvChad
+
+`sudo snap install nvim --classic`
+
+`git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1`
